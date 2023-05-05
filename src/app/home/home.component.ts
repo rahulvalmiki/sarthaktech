d8883948b8ed3d17c252468d7ec0ca8df0f1e733
rahulvalmiki <rahulvalmiki41@gmail.com>
2023-05-05 15:20:03 +0530

    add file

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SwiperOptions } from 'swiper';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
+
